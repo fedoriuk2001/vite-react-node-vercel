8b0368c3df0b462dae2b6b45891343b27babc0e4
Eugen Fedoriuc <eugen.fedoriuc@kwb.at>
2024-02-06 12:43:01 +0100

    added variable

--- server/index.js
+++ server/index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use("/", (req, res) => {
-  res.send("Server is runnning.");
-  res.send("My password: ", process.env.HELLO_BOY); 
+  res.send("Server is runnning. Bla bla bla", process.env.HELLO_BOY);
 });
 
 app.listen(5000, () => {
