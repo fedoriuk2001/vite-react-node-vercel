0725749abc407a474a11638b976c26e256438f5d
Eugen Fedoriuc <eugen.fedoriuc@kwb.at>
2024-02-06 13:12:05 +0100

    added cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.use(cors());
+
 app.get("/", (req, res) => {
   const accessKey = process.env.HELLO_BOY;
   
   res.send(`Server is running. HELLO_BOY: ${accessKey}`);
 });
+
 
 // Start the server on port 5000
 app.listen(5000, () => {
