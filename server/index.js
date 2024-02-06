b3cc52519bc3aac886b9ab02bae51154f0fb9f7b
Eugen Fedoriuc <eugen.fedoriuc@kwb.at>
2024-02-06 12:40:06 +0100

    added HELLO_BOY

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use("/", (req, res) => {
   res.send("Server is runnning.");
-  console.log(process.env.HELLO_BOY); 
+  res.send(process.env.HELLO_BOY); 
 });
 
 app.listen(5000, () => {
