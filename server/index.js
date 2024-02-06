32df519d5cdf9055b6f75a1dab016b71f0385706
Eugen Fedoriuc <eugen.fedoriuc@kwb.at>
2024-02-06 12:41:46 +0100

    added variable

--- server/index.js
+++ server/index.js
@@ -3,11 +3,10 @@ const app = express();
 
 app.use("/", (req, res) => {
   res.send("Server is runnning.");
-  res.send(process.env.HELLO_BOY); 
+  res.send("My password: ", process.env.HELLO_BOY); 
 });
 
 app.listen(5000, () => {
     console.log("Server started on port 5000")
-    console.log(process.env.HELLO_BOY); 
     }    
 );
