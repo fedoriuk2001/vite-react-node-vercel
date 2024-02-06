4feb3eceffe198432ef9db38f88940a74125e121
Eugen Fedoriuc <eugen.fedoriuc@kwb.at>
2024-02-06 12:37:29 +0100

    added process.env.HELLO_BOY

--- server/index.js
+++ server/index.js
@@ -5,4 +5,8 @@ app.use("/", (req, res) => {
   res.send("Server is runnning.");
 });
 
-app.listen(5000, console.log("Server started on port 5000"));
+app.listen(5000, () => {
+    console.log("Server started on port 5000")
+    console.log(process.env.HELLO_BOY); 
+    }    
+);
